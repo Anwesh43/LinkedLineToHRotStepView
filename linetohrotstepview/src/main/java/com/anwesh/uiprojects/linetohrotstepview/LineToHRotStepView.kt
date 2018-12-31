265f378b73d1101671c289355f980bdc208ab819
Anwesh43 <anweshthecool0@gmail.com>
2018-12-31 10:00:31 +0530

    adding Float and Int extension methods

--- linetohrotstepview/src/main/java/com/anwesh/uiprojects/linetohrotstepview/LineToHRotStepView.kt
+++ linetohrotstepview/src/main/java/com/anwesh/uiprojects/linetohrotstepview/LineToHRotStepView.kt
@@ -20,3 +20,11 @@ val scDiv : Double = 0.51
 val scGap : Float = 0.05f
 val backColor : Int = Color.parseColor("#212121")
 val strokeColor : Int = Color.parseColor("#f44336")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse()).toFloat()
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * scGap * dir
+

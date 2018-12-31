251a2e86217a6970950393cea582fe89394f87fc
Anwesh43 <anweshthecool0@gmail.com>
2018-12-31 09:56:25 +0530

    adding constants and importing libraries

--- linetohrotstepview/src/main/java/com/anwesh/uiprojects/linetohrotstepview/LineToHRotStepView.kt
+++ linetohrotstepview/src/main/java/com/anwesh/uiprojects/linetohrotstepview/LineToHRotStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.linetohrotstepview
 /**
  * Created by anweshmishra on 31/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.7f
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+val backColor : Int = Color.parseColor("#212121")
+val strokeColor : Int = Color.parseColor("#f44336")

ff0dcf7b11302cd2b17505ad714f00efb7b36ffc
Anwesh43 <anweshthecool0@gmail.com>
2018-12-31 10:21:03 +0530

    adding LHRSNode class

--- linetohrotstepview/src/main/java/com/anwesh/uiprojects/linetohrotstepview/LineToHRotStepView.kt
+++ linetohrotstepview/src/main/java/com/anwesh/uiprojects/linetohrotstepview/LineToHRotStepView.kt
@@ -119,4 +119,48 @@ class LineToHRotStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LHRSNode(var i : Int, val state : State = State()) {
+
+        private var next : LHRSNode? = null
+        private var prev : LHRSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LHRSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLHRSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LHRSNode {
+            var curr : LHRSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
